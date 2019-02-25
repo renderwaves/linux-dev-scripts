43ebf9ce2e8ef588be4bbb6ba8bece506c5152e9
renderwavesteam <team@renderwaves.com>
2019-02-25 23:55:54 +0100

    second update

--- pre_build_vm.sh
+++ pre_build_vm.sh
@@ -12,69 +12,37 @@
 # 5. setup .profile env
 # Ready to work with
 
-###############
-# env variables
-###############
-DL_ANDROID_STUDIO="https://dl.google.com/dl/android/studio/ide-zips/3.3.1.0/android-studio-ide-182.5264788-linux.zip"
-#R9 ONLY FOR NOW
-DL_ANDROID_NDK="http://dl.google.com/android/ndk/android-ndk-r9d-linux-x86_64.tar.bz2"
-DL_ANDROID_TOOLS=""
-DL_EMSCRIPTEN=""
+# FOR INTERNSHIP STUDENTS:
+# -----------------------
+# TODO: install_packages()
+# 	Write func to install packages from array
+#	Eg: 	Input Array linux-headers-$(uname -r) gcc g++ make scons ccache libgl1-mesa-dev libxrandr-dev libxinerama-dev libopenal1 libxrender-dev libxext-dev libc6-dev libx11-dev
+#	    	Result: sudo apt-get install {$ARRAY}
 
-################################################
-# Global VM prepare packages for development env
-################################################
-sudo apt-get install -y
-linux-headers-$(uname -r) \
-mc \
-ssh \
-gcc \
-g++ \
-make \
-scons \
-ccache\
-libgl1-mesa-dev \
-libxrandr-dev \
-libxinerama-dev \
-libopenal1 \
-libxrender-dev \
-libxext-dev \
-libc6-dev \
-libx11-dev
+# TODO: create_folders()
+#	Write function for generating folder structure
+#	Eg: 	~/git
+#		~/Toolchains
+#		~/Toolchains/Android
+#		~/Toolchains/Android/ndk
+#		~/Toolchains/Android/sdk
+#		~/Toolchains/Android/tool
+#		~/Toolchains/Android/studio
 
-########################
-# download packages
-#########################
-cd ~/Downloads
-wget $DL_ANDROID_STUDIO
-wget $DL_ANDROID_NDK
-wget $DL_ANDROID_TOOLS
-wget $DL_EMSCRIPTEN
+# TODO: get_extract_archive()
+#	Write func to obtaining tar/zip archive and extracting it in specified folder
+#	Eg: Zip android-studio-ide-182.5264788-linux.zip downloaded into ~/Downloads and extracted into ~/Toolchains/Android/studio
 
-########################
-# setup folder structure
-########################
-cd ~
-# GIT 
-mkdir ~/git
-mkdir ~/git/build
+# TODO: get_file_from_url()
+#	Write func to tokenize url string and separate file + extension from url
+#	Eg: Source: https://dl.google.com/dl/android/studio/ide-zips/3.3.1.0/android-studio-ide-182.5264788-linux.zip
+#	    Result: android-studio-ide-182.5264788-linux.zip
 
-# TOOLCHAINS
-mkdir ~/Toolchains
-# ANDROID
-mkdir ~/Toolchains/Android
+# TODO: check_existing_env()
+#	Write func for checking environment variable in .profile 
+#	Eg: Function will be looking into .profile and checking if some our predefined export variables already exists
 
-mkdir ~/Toolchains/Android/studio
-tar -x ~/Downloads/$DL_ANDROID_STUDIO ~/Toolchains/Android/studio
+# TODO: write_env()
+#	Write func for writing an envrionment variable into .profile
 
-mkdir ~/Toolchains/Android/ndk
-mkdir ~/Toolchains/Android/sdk
-mkdir ~/Toolchains/Android/tools
-# WEBGL
-mkdir ~/Toolchains/webgl
-# RASPBERRYPI
-mkdir ~/TOolchains/raspberry
-
-# write to profile env
-
-# at the end do not forget to clone repositories
+# Execute script depending on arguments
